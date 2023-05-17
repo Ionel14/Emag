c803e1be7c05bc52cfac037d76dfb4c6bdfe16eb
Ionel14 <53469111+Ionel14@users.noreply.github.com>
2023-05-17 21:07:18 +0300

    format

--- lib/src/models/products/products_state.dart
+++ lib/src/models/products/products_state.dart
@@ -7,6 +7,7 @@ class ProductsState with _$ProductsState {
     @Default(<Category>[]) List<Category> categories,
     @Default(<Vendor>[]) List<Vendor> vendors,
     String? selectedCategoryId,
+
   }) = ProductsState$;
 
   factory ProductsState.fromJson(Map<dynamic, dynamic> json) =>
